39abb1a911fda61b486dc1c422ed8f81c96882aa
Daniel Lerch Hostalot <dlerch@gmail.com>
2018-05-06 15:43:24 +0200

    encryption

--- stc.py
+++ stc.py
@@ -6,15 +6,41 @@ import math
 import random
 from ctypes import *
 from PIL import Image
+from Crypto.Cipher import AES
+from Crypto import Random
 
-def to_bits(filename):
+def read_and_encrypt(filename, key):
     array=[]
     f=open(filename, 'r')
-    bytes = (ord(b) for b in f.read())
+    aes = AESCipher(key)
+    enc = aes.encrypt(f.read())
+    bytes = (ord(b) for b in enc)
     for b in bytes:
         for i in xrange(8):
             array.append((b >> i) & 1)
     return array
+
+
+class AESCipher:
+    def __init__( self, key ):
+        self.key = key
+        BS = 16
+        self._pad = lambda s: s + (BS - len(s) % BS) * chr(BS - len(s) % BS) 
+        self._unpad = lambda s : s[:-ord(s[len(s)-1:])]
+
+    def encrypt( self, raw ):
+        raw = self._pad(raw)
+        iv = Random.new().read( AES.block_size )
+        cipher = AES.new( self.key, AES.MODE_CBC, iv )
+        return iv + cipher.encrypt( raw ) 
+
+    def decrypt( self, enc ):
+        iv = enc[:16]
+        cipher = AES.new(self.key, AES.MODE_CBC, iv )
+        enc = self._pad(enc)
+        return self._unpad(cipher.decrypt( enc[16:] ))
+
+
 
 def embed(input_img_path, msg_file_path, output_img_path, payload=0.40):
 
@@ -54,7 +80,7 @@ def embed(input_img_path, msg_file_path, output_img_path, payload=0.40):
             costs[3*i+2] = 1
 
     # Prepare message
-    msg_bits = to_bits(msg_file_path)
+    msg_bits = read_and_encrypt(msg_file_path, "0123456789012345")
     print "msg len:", len(msg_bits)
     if len(msg_bits)>width*height*payload:
         print "Message too long"
@@ -108,81 +134,22 @@ def extract(stego_img_path, output_msg_path, payload=0.40):
     s = lib.stc_unhide(n, stego, m, extracted_message)
 
     # Save the message
-    f = open(output_msg_path, 'w')
+    enc = ''
     idx=0
     bitidx=0
     bitval=0
     for b in extracted_message:
         if bitidx==8:
-            f.write(chr(bitval))
+            enc += chr(bitval)
             bitidx=0
             bitval=0
         bitval |= b<<bitidx
         bitidx+=1
+
+    aes = AESCipher("0123456789012345")
+    f = open(output_msg_path, 'w')
+    f.write(aes.decrypt(enc))
     f.close()
 
 
-
-
-def _embed(input_img_path, msg_file_path, output_img_path, payload=0.40):
-
-    me = os.path.abspath(os.path.dirname(__file__))
-    lib = cdll.LoadLibrary(os.path.join(me, "lib", "stc.so"))
-
-
-    random.seed(1)
-    payload = 0.1
-
-    INF = 2**31-1
-
-    n = 512*512
-    cover = (c_int*n)()
-    costs = (c_float*(n*3))()
-    for i in range(n):
-        cover[i] = random.randint(0,255)
-        if cover[i]==0:
-            costs[3*i+0] = INF # INF
-            costs[3*i+1] = 0
-            costs[3*i+2] = 1
-        elif cover[i]==255:
-            costs[3*i+0] = 1   
-            costs[3*i+1] = 0 
-            costs[3*i+2] = INF # INF
-        else:
-            costs[3*i+0] = 1
-            costs[3*i+1] = 0
-            costs[3*i+2] = 1
-
-
-    m = int(math.floor(payload*n)) 
-    message = (c_ubyte*m)()
-
-
-    str_message = "" 
-    for i in range(m):
-        b = random.randint(0,1)
-        message[i] = b
-        str_message += str(b)
-
-    print "message:", str_message [:70]
-    print "message:", str_message [-70:]
-
-
-    stego = (c_int*n)()
-    a = lib.stc_hide(n, cover, costs, m, message, stego)
-    print a, cover[4], stego[4]
-
-    extracted_message = (c_ubyte*m)()
-    s = lib.stc_unhide(n, stego, m, extracted_message)
-
-    str_message = "" 
-    for i in range(m):
-        str_message += str(extracted_message[i])
-    print "message:", str_message [:70]
-    print "message:", str_message [-70:]
-
-    for i in range(m):
-        if message[i] != extracted_message[i]:
-            print "ERROR! different messages"
-            break
 
